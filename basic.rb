89a0517da9023d35f957a2509d28e301462612eb
lucapette <lucapette@gmail.com>
2011-05-14 19:15:11 +0200

    ask for a locale

--- basic.rb
+++ basic.rb
@@ -2,6 +2,11 @@
 
 apply(File.join(File.dirname(__FILE__),"base.rb"))
 
+if yes?("Do you want an additional locale?")
+  locale=ask("which?")
+  get("https://github.com/svenfuchs/rails-i18n/raw/master/rails/locale/#{locale}.yml","config/locales/#{locale}.yml")
+end
+
 if yes?("Would you like to use thin?")
   gem 'thin'
 end
